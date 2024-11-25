ba22dd482581c13f08f338d865163ce0d546c81f
Net test <hryst2092@mail.ru>
2024-11-25 22:47:35 +0300

    add main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
-        System.out.println(2 + 3);
-        System.out.println();
+        int eaters = 5; // сколько людей будут есть
 
-        int speed = 17;
-        System.out.println(speed);
+        int water = 3000; // миллилитров воды
+        int potatoes = 5; // картофелин
+        int chicken = 6; // куриных бёдер
+        int spices = 10; // ложек специй
+
+        System.out.println("Сварили суп. На одного человека вышло:");
+        System.out.println((water / eaters) + " миллилитров(а) воды");
+        System.out.println((potatoes / eaters) + " картофелин(а/ы)");
+        System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
+        System.out.println((spices / eaters) + " ложек(ки/ка) специй");
+
     }
 }
