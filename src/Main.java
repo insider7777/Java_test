f9ca3d6df62718d205ed173707a495d2ccd798d3
Net test <hryst2092@mail.ru>
2024-11-25 22:39:24 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+        System.out.println(2 + 3);
+        System.out.println();
+
+        int speed = 17;
+        System.out.println(speed);
+    }
+}
